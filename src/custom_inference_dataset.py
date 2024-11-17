65d80dfe5c3611522a5e648a762e286ebdcb7672
caiselvass <caiselvassala@gmail.com>
2024-11-17 11:44:47 +0100

    ADD fast inference version

--- src/custom_inference_dataset.py
+++ src/custom_inference_dataset.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 from PIL import Image
 
 class CustomInferenceDataset(Dataset):
-	def __init__(self, label_encoders: Dict, onehot_encoders: Dict, x_test_path: str, image_folder_path: str, filename_col: str='des_filename', test_id_col: str='test_id', attribute_name_col: str='attribute_name'):
+	def __init__(self, data, label_encoders: Dict, onehot_encoders: Dict, image_folder_path: str, filename_col: str='des_filename', test_id_col: str='test_id', attribute_name_col: str='attribute_name'):
 		"""
 		Args:
 			label_encoders (Dict): Dictionary containing the label encoders used in the x_train dataset.
@@ -24,7 +24,7 @@ class CustomInferenceDataset(Dataset):
 		self.image_folder_path = image_folder_path
 		self.label_encoders = label_encoders
 		self.onehot_encoders = onehot_encoders
-		self.data = pd.read_csv(x_test_path)
+		self.data = data
 		self.filename_col = filename_col
 		self.test_id_col = test_id_col
 		self.attribute_name_col = attribute_name_col
@@ -41,6 +41,9 @@ class CustomInferenceDataset(Dataset):
 		self.tabular_data = torch.tensor(self.tabular_data.values, dtype=torch.long)
 
 		self.image_names = self.data[self.filename_col].values
+
+		self.image_prefixes = [''.join(img_name.split('_')[:2]) for img_name in self.image_names]
+
 		self.attribute_categories = self.data[self.attribute_name_col].values
 		self.test_ids = self.data[self.test_id_col].values
 
@@ -63,11 +66,9 @@ class CustomInferenceDataset(Dataset):
 		# Get tabular data and label for the current index
 		tabular_data = self.tabular_data[idx].clone().detach().to(torch.long)
 
-		attribute_name = self.attribute_categories[idx]
+		image_prefix = self.image_prefixes[idx]
 
-		test_id = self.test_ids[idx]
-
-		return image, tabular_data, attribute_name, test_id
+		return image, tabular_data, image_prefix
 	
 	def value_from_label_encoder(self, col: int, label: int) -> str:
 		"""
